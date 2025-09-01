4cbc152c2aeafde420dc0e2a7020a1bc65b17fd4
gorguimarena <gorguimarena@gmail.com>
2025-09-01 21:04:25 +0000

    amelioration des messages

---
+++ src/enums/profile.messages.ts
@@ -0,0 +1,11 @@
+export enum ProfileMessage {
+  Created = "Profil créé avec succès",
+  Updated = "Profil mis à jour avec succès",
+  Deleted = "Profil supprimé avec succès",
+
+  NotFound = "Profil introuvable",
+  CreationFailed = "Échec de la création du profil",
+  UpdateFailed = "Échec de la mise à jour du profil",
+  DeletionFailed = "Échec de la suppression du profil",
+}
+
--- src/modules/profile/profile.controller.ts
+++ src/modules/profile/profile.controller.ts
@@ -5,15 +5,16 @@ import {
   updateProfileSchema,
 } from "./profile.schema.js";
 import BaseController from "../module/module.controller.js";
+import { ProfileMessage } from "../../enums/profile.messages.js";
 
 export default class ProfileController extends BaseController<UpdateProfileInput> {
   protected service = new ProfileService();
   protected createSchema = createProfileSchema;
   protected updateSchema = updateProfileSchema;
   protected messages = {
-    created: "Profil créé avec succès",
-    updated: "Profil mis à jour avec succès",
-    deleted: "Profil supprimé avec succès",
+    created: ProfileMessage.Created,
+    updated: ProfileMessage.Updated,
+    deleted: ProfileMessage.Deleted,
   };
 }
 
