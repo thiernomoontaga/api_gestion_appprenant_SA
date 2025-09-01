0027eda640a738f7343e3c0ed15400449263d633
Thierno <thiernosegnane316@gmail.com>
2025-09-01 09:32:31 +0000

    gestion des conflits

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import express from "express";
 import userRoutes from "./modules/users/user.routes";
 import profilSortieRoutes from "./modules/profilsorties/profilsortie.routes";
 import swaggerDoc from "./middlewares/swagger";
+import competenceRouter from "./modules/competence/competence.routes.js";
+
 
 const app = express();
 
@@ -11,5 +13,9 @@ app.use(swaggerDoc);
 
 app.use("/utilisateurs", userRoutes);
 app.use("/profilsorties", profilSortieRoutes);
+app.use("/competences", competenceRouter);
 
 export default app;
+
+
+
