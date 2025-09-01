c30e7fc42e39ff7a940a4c60954bfcd66d7f108c
gorguimarena <gorguimarena@gmail.com>
2025-09-01 09:35:00 +0000

    revue controllers

---
+++ src/config/swagger.ts
@@ -0,0 +1,27 @@
+// @ts-ignore
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+import { Express } from "express";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Gestion Apprenants",
+      version: "1.0.0",
+      description: "Documentation de l'API Gestion Apprenants avec Swagger",
+    },
+    servers: [
+      {
+        url: "http://localhost:4000",
+      },
+    ],
+  },
+  apis: ["./src/modules/**/*.ts"],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+
+export const setupSwagger = (app: Express) => {
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
